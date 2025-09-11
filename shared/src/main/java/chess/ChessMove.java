419e1d55ed9aa058d5c85223b60f507d3a4689b3
NealSmalley <neal.e.smalley@gmail.com>
2025-09-11 15:48:47 -0600

    added toString functions to ChessMove and ChessPosition

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -45,6 +45,11 @@ public class ChessMove {
     }
 
     @Override
+    public String toString() {
+        return String.format("%s%s", startPosition, endPosition);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) {
             return false;
